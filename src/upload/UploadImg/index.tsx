3a612387d9a6f963a0a701b93b65f3ea711d36ef
gisele <17761608@qq.com>
2023-06-02 15:29:30 +0800

    ✨ feat: add dependencies4

--- src/upload/UploadImg/index.tsx
+++ src/upload/UploadImg/index.tsx
@@ -1,6 +1,15 @@
-import React, { memo, useState } from 'react'
-import { Upload } from 'antd'
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
+import React, { memo, useState } from "react"
+import { Upload, Modal } from "antd"
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons"
+
+const toInitImg = (data) => {
+  const list = data?.split(",") || []
+  const value: any = []
+  list.forEach((_, index) => {
+    value.push({ uid: index + 1, status: "done", url: _ })
+  })
+  return value
+}
 
 /**
  * @name  上传图片等
@@ -14,45 +23,66 @@ export const MidUploadImg = memo((props: any) => {
     message,
     className,
     typeName,
-    value,
+    value: initValue,
     limits,
     uploadUrl,
     serverUrl,
     headers,
     onChange,
-    tip = null
+    tip = null,
+    maxCount = 1
   } = props
   const [loading, setLoading] = useState(false)
+  const [value, setValue] = useState(toInitImg(initValue))
+  const [preview, setPreview] = useState({
+    open: false,
+    url: ""
+  })
 
   const beforeUpload = (file: any) => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       const { maxSize, fileType } = limits
       if (fileType && file.type.indexOf(fileType) < 0) {
         message(LANG.IMG_TIP_TYPE)
-        reject()
+        return Upload.LIST_IGNORE
       }
       if (file.size > maxSize * 1024 * 1024) {
         message(LANG.IMG_TIP_SIZE(typeName, maxSize))
-        reject()
+        return Upload.LIST_IGNORE
       }
       resolve(true)
     })
   }
 
-  const handleChange = (info: any) => {
-    const { file } = info
-    const { status, response } = file
-    if (status === 'uploading') {
-      setLoading(true)
-    } else if (status === 'done') {
-      setLoading(false)
-      let { code, msg, data } = response
-      if (code === '8001') {
-        onChange(data && data.path ? data.path : data)
-      } else {
-        message(msg)
+  const handleChange = ({ fileList }) => {
+    const value: any = []
+    const urlList: any = []
+    fileList.forEach((_) => {
+      let { uid, status, name, url = "", response } = _
+      if (status === "uploading") {
+        setLoading(true)
+      } else if (status === "done" && response) {
+        setLoading(false)
+        let { code, msg, data } = response
+        if (code === "8001") {
+          url = serverUrl + (data && data.path ? data.path : data)
+        } else {
+          message(msg)
+        }
       }
-    }
+      value.push({ uid, name, status, url })
+      urlList.push(url)
+    })
+    setValue(value)
+    onChange(urlList.join(","))
+  }
+
+  const handlePreview = (file) => {
+    file.url &&
+      setPreview({
+        open: true,
+        url: file.url
+      })
   }
 
   return (
@@ -62,20 +92,29 @@ export const MidUploadImg = memo((props: any) => {
         listType="picture-card"
         action={uploadUrl}
         headers={headers}
-        maxCount={1}
-        // @ts-ignore
+        onPreview={handlePreview}
         beforeUpload={beforeUpload}
         onChange={handleChange}
+        fileList={value}
       >
-        {value && <img src={serverUrl + value} alt={LANG.IMG} style={{ width: '100%' }} />}
-        {!value && (
+        {value.length < maxCount && (
           <>
             {loading ? <LoadingOutlined /> : <PlusOutlined />}
-            <div className="ant-upload-text">{loading ? LANG.UPLOAD_ING : LANG.UPLOAD}</div>
+            <div className="ant-upload-text">
+              {loading ? LANG.UPLOAD_ING : LANG.UPLOAD}
+            </div>
           </>
         )}
       </Upload>
       {tip}
+      <Modal
+        open={preview.open}
+        footer={null}
+        title="图片预览"
+        onCancel={() => setPreview({ open: false, url: "" })}
+      >
+        <img alt="example" style={{ width: "100%" }} src={preview.url} />
+      </Modal>
     </div>
   )
 })
